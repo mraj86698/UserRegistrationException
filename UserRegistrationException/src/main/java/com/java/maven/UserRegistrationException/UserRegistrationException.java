07bb183f736ba6150cafd18c8a8bbb51f8028ef3
MohanRaj <mraj86698@gmail.com>
2022-08-22 19:22:57 +0530

    Added to Welcome Message

---
+++ UserRegistrationException/src/main/java/com/java/maven/UserRegistrationException/UserRegistrationException.java
@@ -0,0 +1,13 @@
+package com.java.maven.UserRegistrationException;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserRegistrationException
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to UserRegistration Exception" );
+    }
+}
---
+++ UserRegistrationException/src/test/java/com/java/maven/UserRegistrationException/UserRegistrationExceptionTest.java
@@ -0,0 +1,20 @@
+package com.java.maven.UserRegistrationException;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserRegistrationExceptionTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
